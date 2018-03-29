8436c31d0fd564d817b68a056b29591849313662
Jacob Howarth <jshowa.git@gmail.com>
2018-03-28 21:37:23 -0400

    Completed first 2 and a half steps in Roll a Ball tutorial.

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Game code
+    }
+
+    private void FixedUpdate()
+    {
+        // Physics code
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
